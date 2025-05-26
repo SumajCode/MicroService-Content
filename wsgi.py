7fd73b7183e11dbe1888728597b141790bd5f0ca
rodrixhub <rodriterminator69@gmail.com>
2025-05-26 01:14:04 -0400

    Arreglado wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
+import sys
 import os
+
+# Agregar la carpeta src al path para que se puedan importar los módulos correctamente
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+
 from app import create_app
 
-# Crear la aplicación para el servidor WSGI
 application = create_app()
 
 if __name__ == "__main__":
