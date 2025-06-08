e8dad2bb2665029cc53008a1835a30dd6fa01d81
rodrixhub <rodriterminator69@gmail.com>
2025-06-08 01:39:16 -0400

    Arreglo de wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,6 @@
 import sys
 import os
 
-# Agregar la carpeta src al path para que se puedan importar los módulos correctamente
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
 from app import create_app
 
 application = create_app()
