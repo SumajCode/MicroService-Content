8a2751d6f664eaf0817c8289a5d6bb902ba1d247
raulpsl <raulparedesld@gmail.com>
2025-06-24 08:54:29 -0400

    update: for production

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'
 
 from src.app import crearApp
 
-application = crearAppy()
+application = crearApp()
 
 if __name__ == "__main__":
     application.run()
