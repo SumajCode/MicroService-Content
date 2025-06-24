7e6c90833a39e20b8cd607f057757881fc9b9879
raulpsl <raulparedesld@gmail.com>
2025-06-24 08:54:15 -0400

    update: for production

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,9 @@ import os
 # Agregar la carpeta src al path para que se puedan importar los módulos correctamente
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
-from app import create_app
+from src.app import crearApp
 
-application = create_app()
+application = crearAppy()
 
 if __name__ == "__main__":
     application.run()
